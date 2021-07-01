de11cc8e7ccc5a7f1bb20376b5350f444b130b5e
temp192000 <84807334+temp192000@users.noreply.github.com>
2021-07-01 23:14:17 +0530

    equality in JavaScript.

---
+++ lecture-07/equality/script.js
@@ -0,0 +1,17 @@
+console.log(" " == 0)
+console.log(" " === 0)
+
+console.log("" == "")
+console.log(" " == 0)
+
+console.log("\n" == 0)
+console.log("\t" == 0)
+console.log("\n" == "\t")
+
+console.log(false == 0)
+console.log(false === 0)
+
+console.log(" " == false)
+console.log("false" == false)
+console.log("false" === false)
+
