df65350e34eb4bd336d6120c572518358be855af
temp192000 <84807334+temp192000@users.noreply.github.com>
2021-07-01 23:14:54 +0530

    string literals.

---
+++ lecture-07/string-literals/script.js
@@ -0,0 +1,20 @@
+let s1 = 'This is single quoted String.'
+
+let s2 = `
+This is a back-ticked string inside
+`
+let s3 = "This is a double quoted String."
+
+let s4 = "This is a 'single quoted string' inside double quoted string."
+
+let s5 = 'This is "double quoted string" inside a single quoted string.'
+
+let s6 = `This is 'single quoted' and "double quoted" string inside a back-ticked string.`
+
+let number = 12;
+
+let s7 = 'This notation : ${number} would not work in single quoted string literal.'
+
+let s8 = "Also, This notation : ${number} would not work in double quoted string literal."
+
+let s9 = `The $ notation: ${number} would work in back-ticked string literal only.`
