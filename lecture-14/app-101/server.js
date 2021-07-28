6951ff961b882ef079a73fbf5bd154b96aaede45
temp192000 <codemonkey.ranjith@gmail.com>
2021-07-29 01:08:01 +0530

    Query Parameters.

--- lecture-14/app-101/server.js
+++ lecture-14/app-101/server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hewwo, World!")
+    res.send("Hewwo, This is a Server Page!")
+})
+
+app.get('/welcome', (req, res) => {
+    let name = 'Guest';
+    if(req.query.user) name = req.query.user;
+    res.send(`Hewwo, ${name}!`)
 })
 
 app.use('/static', express.static(__dirname + '/public'))
