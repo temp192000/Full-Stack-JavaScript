d8c487916e8b6b605ad45e1fc36eb07b3222fb76
temp192000 <codemonkey.ranjith@gmail.com>
2021-09-13 22:40:14 +0530

    Express 101

---
+++ clips/express-101/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+// console.log(typeof express); //a function
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color: blue;">Hewwo</h1>')
+})
+
+
+app.listen(4444, () => {
+    console.log("Server Started on http://localhost:4444");
+    console.log("Server Started on http://0.0.0.0:4444");
+    console.log("Server Started on http://127.0.0.1:4444");
+    console.log("Server Started on http://<IP Address>:4444");
+})
