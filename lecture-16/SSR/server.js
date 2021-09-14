cfe6a963d6f961fa65d0284e9e0b1604d5801351
temp192000 <codemonkey.ranjith@gmail.com>
2021-09-14 15:46:49 +0530

    Minimal Server Boilerplate

---
+++ lecture-16/SSR/server.js
@@ -0,0 +1,14 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("Hewwo!")
+});
+
+app.listen(4444, () => {
+    console.log('Server Started on http://localhost:4444');
+})
