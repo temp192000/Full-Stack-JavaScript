eafa4dcb07a09797ee8f72d761c7d8f602cb83f9
temp192000 <codemonkey.ranjith@gmail.com>
2021-09-14 16:46:40 +0530

    CSR vs SSR

--- lecture-16/SSR/server.js
+++ lecture-16/SSR/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
+const appRoute = require('./routes/app');
+
+app.set('view engine', 'hbs');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-    res.send("Hewwo!")
-});
+app.use('/taskmanager', appRoute)
 
 app.listen(4444, () => {
     console.log('Server Started on http://localhost:4444');
